cca4ba27902fce39424e257be00437c25a6bf513
TijmenvandeMerbel <106590970+TijmenvandeMerbel@users.noreply.github.com>
2022-06-02 09:51:34 +0200

    Update regelaar.py

---
+++ universal_robots_ws/src/my_universal_robots/transportsysteem/scripts/regelaar.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+import rospy
+
+
+
+# importing packages
+# Publischer die een empty message stuurt naar topic/start_band
+# Subscriber die luisterd naar positie_band van massage type std_msgs/float32
+# Als die waardes tussen 4,99 en 5,01 moet hij iets uitschrijven
